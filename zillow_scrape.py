513565cd7cd1154894326c95ce0394ddbd921d3d
trevornei <117675657+trevornei@users.noreply.github.com>
2024-10-09 12:56:07 -0600

    Added three user agent strings to disguise the get request.

--- zillow_scrape.py
+++ zillow_scrape.py
@@ -1,12 +1,18 @@
 import requests
-
+import random
 from bs4 import BeautifulSoup
 
 # URL that we want to scrape.
-url = "https://www.zillow.com/missoula-mt/"
+url = "https://www.zillow.com/missoula-mt/homes/for_sale/"
+
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+]
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+    'User-Agent': random.choice(user_agents)
 }
 
 # Get request to the URL
